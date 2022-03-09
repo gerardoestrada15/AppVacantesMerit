b40581886f1eb998b7508e0059a317d4f0ad9ce3
gerardoestrada15 <37393186+gerardoestrada15@users.noreply.github.com>
2022-03-09 10:42:13 -0800

    Add files via upload

---
+++ web/index.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+
+def hola():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/admincandidatos')
+def admincandidatos():
+    return render_template('admincandidatos.html')
+
+@app.route('/centromensajes')
+def centro():
+    return render_template('centromensajes.html')
+
+@app.route('/actividades')
+def actividades():
+    return render_template('actividades.html')
+
+if __name__== '__main__':
+    app.run(debug=True)
