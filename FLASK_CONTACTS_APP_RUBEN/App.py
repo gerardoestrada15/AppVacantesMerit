213d2b59b5693631fb681ebfc00a6fada82bc670
RubenUrias <101018165+RubenUrias@users.noreply.github.com>
2022-04-24 21:53:33 -0700

    Update App.py

--- FLASK_CONTACTS_APP_RUBEN/App.py
+++ FLASK_CONTACTS_APP_RUBEN/App.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
-
 app = Flask(__name__)
-
 
 app.config['MYSQL_HOST'] = 'localhost' 
 app.config['MYSQL_USER'] = 'root'
@@ -20,7 +18,7 @@ def Index():
     cur.execute('SELECT * FROM contacts')
     data = cur.fetchall()
     cur.close()
-    return render_template('index.html', contacts = data)
+    return render_template('adminvacantes.html', contacts = data)
 
 @app.route('/add_contact', methods=['POST'])
 def add_contact():
@@ -32,7 +30,7 @@ def add_contact():
         cur.execute('INSERT INTO contacts (vacante, descripcion,originario) VALUES (%s,%s,%s)',
         (vacante,descripcion,originario))
         mysql.connection.commit()
-        flash('Contact Added successfully')
+       
         return redirect(url_for('Index'))
 
 @app.route('/edit/<id>', methods = ['POST', 'GET'])
@@ -58,7 +56,7 @@ def update_contact(id):
                 originario = %s
             WHERE id = %s
         """, (vacante,descripcion,originario, id))
-        flash('Vacante Actualizada Correctamente')
+        
         mysql.connection.commit()
         return redirect(url_for('Index'))
 
@@ -67,7 +65,7 @@ def delete_contact(id):
     cur = mysql.connection.cursor()
     cur.execute('DELETE FROM contacts WHERE id = {0}'.format(id))
     mysql.connection.commit()
-    flash('Vacante Removida Correctamente')
+    
     return redirect(url_for('Index'))
 
 @app.route('/añadir/<id>', methods = ['POST', 'GET'])
@@ -79,7 +77,36 @@ def añadir_contact(id):
     print(data[0])
     return render_template('añadir.html', contact = data[0])
 
+def login():
+    return render_template('login.html')
 
+@app.route('/msjpostulantes')
+def msjpostulantes():
+    return render_template('msjpostulantes.html')
+
+@app.route('/adminvacantes')
+def adminvacantes():
+    return render_template('adminvacantes.html')
+
+@app.route('/admincandidatos')
+def admincandidatos():
+    return render_template('admincandidatos.html')
+
+@app.route('/centro')
+def centro():
+    return render_template('centromensajes.html')
+
+@app.route('/actividades')
+def actividades():
+    return render_template('actividades.html')
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
